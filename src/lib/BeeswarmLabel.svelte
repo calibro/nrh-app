f8c5b289000d980bd7319bb02801d4f176037436
uf0 <scnepa222@gmail.com>
2025-11-19 12:58:52 +0100

    fixed height

--- src/lib/BeeswarmLabel.svelte
+++ src/lib/BeeswarmLabel.svelte
@@ -4,16 +4,18 @@
 	const padding = 4;
 </script>
 
-<rect
-	x={-padding}
-	y={-padding}
-	rx={padding}
-	ry={padding}
-	transform={`translate(0, -2)`}
-	width={(contentRect?.width || 0) + padding * 2}
-	height={(contentRect?.height || 0) + padding * 2}
-	fill="#737070"
-></rect>
-<text bind:contentRect fill="white" font-size="0.85rem" x={0} y={0} dominant-baseline="hanging"
-	>{key || 'Not set'} [{count}]</text
->
+<g transform={`translate(${padding * 3}, ${padding * 4})`}>
+	<rect
+		x={-padding}
+		y={-padding}
+		rx={padding}
+		ry={padding}
+		transform={`translate(0, -2)`}
+		width={(contentRect?.width || 0) + padding * 2}
+		height={(contentRect?.height || 0) + padding * 2}
+		fill="#737070"
+	></rect>
+	<text bind:contentRect fill="white" font-size="0.85rem" x={0} y={0} dominant-baseline="hanging"
+		>{key || 'Not set'} [{count}]</text
+	>
+</g>
